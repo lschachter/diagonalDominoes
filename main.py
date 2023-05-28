f7b82057da6ec0b1d28843b45f64801ad75eb345
Lillie Schachter <lillieschachter@gmail.com>
2023-05-28 14:34:56 +0200

    clean naming and run black

--- main.py
+++ main.py
@@ -2,38 +2,40 @@ from grid import *
 from playerCollection import *
 from gamePlay import *
 
+
 def main():
-    #creates the splash screen
-    win=GraphWin("Diagonal Dominoes",1000,600)
-    win.setCoords(0,600,1000,0)
-    win.setBackground("gray")
-    text=Text(Point(500,120),"Diagonal Dominoes")
-    text.draw(win)
+    # creates the splash screen
+    window = GraphWin("Diagonal Dominoes", 1000, 600)
+    window.setCoords(0, 600, 1000, 0)
+    window.setBackground("gray")
+    text = Text(Point(500, 120), "Diagonal Dominoes")
+    text.draw(window)
     text.setSize(36)
-    gameB=Button(win,Point(350,300),200,150,'black',"Play Game")
-    gameB.setTextColor('white')
+    gameB = Button(window, Point(350, 300), 200, 150, "black", "Play Game")
+    gameB.setTextColor("white")
     gameB.setTextSize(24)
-    instB=Button(win,Point(650,300),200,150,'black',"Instructions")
-    instB.setTextColor('white')
+    instB = Button(window, Point(650, 300), 200, 150, "black", "Instructions")
+    instB.setTextColor("white")
     instB.setTextSize(24)
-    x=100
-    y=500
+    x = 100
+    y = 500
     for i in range(6):
-        c1=color_rgb(randrange(255),randrange(255),randrange(255))
-        c2=color_rgb(randrange(255),randrange(255),randrange(255))
-        tile = Tile(c1,c2,win)
-        tile.drawTile(Point(x,y),Point(x+50,y))
-        x+=150
-    pt=win.getMouse()
-    #until the playgame button is clicked, gets new clicks and/or displays
-    #instructions
+        c1 = color_rgb(randrange(255), randrange(255), randrange(255))
+        c2 = color_rgb(randrange(255), randrange(255), randrange(255))
+        tile = Tile(c1, c2, window)
+        tile.drawTile(Point(x, y), Point(x + 50, y))
+        x += 150
+    pt = window.getMouse()
+    # until the playgame button is clicked, gets new clicks and/or displays
+    # instructions
     while not gameB.isClicked(pt):
         if instB.isClicked(pt):
-            instRect=Rectangle(Point(245,100),Point(755,375))
-            instRect.setFill('white')
-            instRect.draw(win)
-            instMess=Text(Point(530,230),
-            "In this two-person game, each player gets five tiles, each with\
+            instRect = Rectangle(Point(245, 100), Point(755, 375))
+            instRect.setFill("white")
+            instRect.draw(window)
+            instMess = Text(
+                Point(530, 230),
+                "In this two-person game, each player gets five tiles, each with\
             \ntwo colors (as below). The first tile chosen is placed \
             \nhorizontally on the bottom-left corner of the board.\
             \nThe next player must place a tile one slot above it and one\
@@ -41,37 +43,33 @@ def main():
             \nthe right-most color of the last tile placed on the board. The\
             \ngame ends when either a player has run out of tiles (they win),\
             \nor has no more valid moves to make (they lose). Give it a try\
-            \nand it will start to make sense. Have fun!\n\n(click to return)")
-            instMess.draw(win)
+            \nand it will start to make sense. Have fun!\n\n(click to return)",
+            )
+            instMess.draw(window)
             instMess.setSize(18)
-            win.getMouse()
+            window.getMouse()
             instRect.undraw()
             instMess.undraw()
-        pt=win.getMouse()
+        pt = window.getMouse()
 
-    #creates the game grid
+    # creates the game grid
     gameB.die()
     instB.die()
-    grid = Grid(win)
-    win.setBackground("gray")
+    grid = Grid(window)
+    window.setBackground("gray")
     grid.drawGrid()
-    quitB=grid.getQuitB()
+    quitB = grid.getQuitB()
 
-    #sets up each player collection
-    player1=PlayerCollection(win,Point(120,50),1)
-    player2 = PlayerCollection(win,Point(880,50),2)
+    # sets up each player collection
+    player1 = PlayerCollection(window, Point(120, 50), 1)
+    player2 = PlayerCollection(window, Point(880, 50), 2)
     player1.displayTiles()
     player1.humanSetup()
     player2.displayTiles()
 
-    #starts the game 
-    game=GamePlay(win,grid,player1,player2,quitB)
+    # starts the game
+    game = GamePlay(window, grid, player1, player2, quitB)
     game.playGame()
 
-       
 
 main()
-
-
-
-
